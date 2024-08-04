fbb09f034fd6edb3a4c1a2e6a56c015d1d8b47ce
Sonali1505 <sonaliwaghmare851@gmail.com>
2024-08-04 15:18:49 +0530

    RightTriangle

---
+++ Pattern/RightTrianglePattern.java
@@ -0,0 +1,11 @@
+public class RightTrianglePattern{
+    public static void main(String args[]){
+        int m=6,n=6;
+        for(int i=0;i<=m;i++){
+            for(int j=0;j<i;j++){
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+}
